cfc3737d343fa34939cc474d51a9f790cd28c040
Gabriel Martins <gabrielmonte.aluno.institute@gmail.com>
2024-06-27 19:22:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Projeto bancário em Java
+
+Iniciando um projeto bancário para entender sobre o paradigma orientado a objetos.
---
+++ src/Accounts/Account.java
@@ -0,0 +1,8 @@
+package Accounts;
+
+// Balance, Bank, Account Number and Owner
+public class Account {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
